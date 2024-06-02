b470979da82e70356db1f8c5fbc43d28407c1a57
Abelmekonn <Abelmekonn9@gmail.com>
2024-06-02 16:04:48 +0300

    "Added react-router and react-router-dom dependencies, updated package-lock.json and package.json, and modified App.jsx to import and render AddEmployee component"

---
+++ demoapp/client/src/pages/AddEmployee.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function AddEmployee() {
+    return (
+        <div>
+            <h1>Add employee</h1>
+            <form>
+                <label htmlFor="fname">First name:</label><br />
+                <input type="text" id="fname" name="fname" /><br />
+                <label htmlFor="lname">Last name:</label><br />
+                <input type="text" id="lname" name="lname" /><br />
+                <label htmlFor="email">Email:</label><br />
+                <input type="text" id="email" name="email" /><br />
+                <label htmlFor="password">Password:</label><br />
+                <input type="text" id="password" name="password" /><br /><br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default AddEmployee
